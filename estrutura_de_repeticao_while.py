f4fea6c499d84977e3c5e6e15209a36f2b5712f7
sargentorw-hub <sargentorw@gmail.com>
2026-08-17 21:05:18 -0400

    Add files via upload

---
+++ estrutura_de_repeticao_while.py
@@ -0,0 +1,16 @@
+condicao = 3
+
+while condicao <301:
+    print (condicao)
+    condicao = condicao + 1
+
+
+    portal_atual =20
+    while portal_atual <=25:
+        portal_atual +=1
+        print ("depois" ,portal_atual)
+        if portal_atual == 22:
+            print("Scaneando_porta :", portal_atual)
+        if portal_atual == 23:
+            continue
+        print("Scaneando_porta :", portal_atual)
